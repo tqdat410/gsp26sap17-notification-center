1f6dcb170136536a1d9b1b46facf5c00cd08daa9
tqdat410 <danhlagi01472@gmail.com>
2026-02-26 10:45:17 +0700

    fix: resolve standalone white screen bootstrap and shell guard

--- webapp/util/CrossAppNavigation.js
+++ webapp/util/CrossAppNavigation.js
@@ -6,16 +6,29 @@
  */
 sap.ui.define([
     'sap/m/MessageToast',
-    'sap/m/MessageBox',
-    'sap/ushell/Container'
-], function (MessageToast, MessageBox, Container) {
+    'sap/m/MessageBox'
+], function (MessageToast, MessageBox) {
     'use strict';
 
     return {
         navigateWithAction: function (sSematicObject, sSematicAction, oParams) {
+            var oContainer;
             var oCrossAppNav;
+            var oService;
+
+            oContainer = sap.ushell && sap.ushell.Container;
+            if (!oContainer) {
+                MessageToast.show('Navigate: ' + sSematicObject + '#' + sSematicAction);
+                return;
+            }
+
             try {
-                oCrossAppNav = Container && Container.getServiceAsync('CrossApplicationNavigation');
+                if (typeof oContainer.getServiceAsync === 'function') {
+                    oCrossAppNav = oContainer.getServiceAsync('CrossApplicationNavigation');
+                } else if (typeof oContainer.getService === 'function') {
+                    oService = oContainer.getService('CrossApplicationNavigation');
+                    oCrossAppNav = Promise.resolve(oService);
+                }
             } catch (e) {
                 MessageToast.show('Navigate: ' + sSematicObject + '#' + sSematicAction);
                 return;
